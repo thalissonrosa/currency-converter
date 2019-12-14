be7079bc379324ff7ce6773754740e27ec6f7fb5
Thalisson da Rosa <darosathalisson@gmail.com>
2019-12-14 16:19:38 -0300

    Initial setup
    Creating the project and making it support iOS 12 and iOS 13.

---
+++ Currency Converter/Currency Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Thalisson da Rosa on 14/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
